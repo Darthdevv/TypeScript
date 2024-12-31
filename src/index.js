4d67918c367ad3ad1f960d3330fb48f390bce8ef
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-12-31 14:11:30 +0200

    💎 feat: required vs partial utility class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 var obj = { a: 5 };
-var obj2 = { a: 5, b: "10" };
+var obj2 = { a: 5 };
 console.log(obj2);
+var obj3 = { a: 5, b: "10" };
+console.log(obj3);
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ interface Props {
 
 const obj: Props = { a: 5 };
 
-const obj2: Required<Props> = { a: 5, b: "10" };
+const obj2: Partial<Props> = { a: 5 };
 console.log(obj2);
+
+const obj3: Required<Props> = { a: 5, b: "10" };
+console.log(obj3);
