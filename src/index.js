4355c50fc5b1cfc6cad7e08968b3f367ee46bfb1
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-01-02 15:16:12 +0200

    💎 feat: Record utility type

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+var users = {
+    yousef: { role: "admin", age: 20 },
+    yasser: { role: "user", age: 30 },
+    nahas: { role: "guest", age: 40 },
+};
+console.log(users.yousef);
 var obj = { a: 5 };
 var obj2 = { a: 5 };
-console.log(obj2);
 var obj3 = { a: 5, b: "10" };
-console.log(obj3);
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,24 @@ interface Props {
   b?: string;
 }
 
+
+type UserName = "yousef" | "yasser" | "nahas";
+
+interface UserInfo {
+  role: string;
+  age: number;
+}
+
+const users: Record<UserName, UserInfo> = {
+  yousef: { role: "admin", age: 20 },
+  yasser: { role: "user", age: 30 },
+  nahas: { role: "guest", age: 40 },
+}
+
+console.log(users.yousef);
+
 const obj: Props = { a: 5 };
 
 const obj2: Partial<Props> = { a: 5 };
-console.log(obj2);
 
 const obj3: Required<Props> = { a: 5, b: "10" };
-console.log(obj3);
