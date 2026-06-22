9aee570c856750447518cb5e80a71ef9781c0583
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2026-06-22 09:58:58 +0300

    changes

--- src/arrays.ts
+++ src/arrays.ts
@@ -4,4 +4,4 @@ const arr1: Readonly<number[]> = [1, 2, 3];
 // arr.push(4); // Error: Property 'push' does not exist on type 'readonly number[]'.
 
 const tuple: readonly [number, string] = [1, "two"];
-// tuple[0] = 2; // Error: Cannot assign to '0' because it is a read-only property. 
+// tuple[0] = 2; // Error: Cannot assign to '0' because it is a read-only property.
